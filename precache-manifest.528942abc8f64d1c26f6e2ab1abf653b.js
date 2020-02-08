cf3e7bbea4d88c74ef167962c5876a55515ea068
Tavish Pegram <tavish@onepeloton.com>
2020-02-08 10:28:40 -0500

    Updates

---
+++ precache-manifest.528942abc8f64d1c26f6e2ab1abf653b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0615121f60e0c40c7ccec06d951fb088",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "461a3cd31b877072aea7",
+    "url": "/monsters-rolodex/static/css/main.2d0f3211.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461a3cd31b877072aea7",
+    "url": "/monsters-rolodex/static/js/main.cdaf800e.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
